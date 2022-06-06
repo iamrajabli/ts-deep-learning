7926111b4f9dbe948979d3ccdfe9b663fadf71da
iamrajabli <iamrajabli@outlook.com>
2022-06-07 01:09:43 +0400

    File update

---
+++ 02Types/Number/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let revenue = 1000;
+let bonus = 500;
+let result = revenue + bonus;
+console.log(result);
---
+++ 02Types/Number/app.ts
@@ -0,0 +1,5 @@
+let revenue:number = 1000;
+let bonus:number  = 500;
+
+let result:number = revenue + bonus;
+console.log(result);
