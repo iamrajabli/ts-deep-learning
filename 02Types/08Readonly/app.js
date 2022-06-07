edae324fe5c55429dff5b874995a5763d0e1076c
iamrajabli <iamrajabli@outlook.com>
2022-06-08 01:53:55 +0400

    File update

--- 02Types/08Readonly/app.js
+++ 02Types/08Readonly/app.js
@@ -1,2 +1,4 @@
 "use strict";
 const users = ['Hikmat', 'Namin', 'Yusif'];
+const ages = [12, 13, 14, 15];
+console.log(users, ages);
--- 02Types/08Readonly/app.ts
+++ 02Types/08Readonly/app.ts
@@ -1,2 +1,4 @@
 const users:readonly string[] = ['Hikmat', 'Namin', 'Yusif'];
+const ages: ReadonlyArray<number> = [12,13,14,15];
 
+console.log(users,ages);
