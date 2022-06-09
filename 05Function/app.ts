67d93d850d8d3ce108f9fc157677a2f4236a2147
iamrajabli <iamrajabli@outlook.com>
2022-06-09 15:19:31 +0400

    File update

--- 05Function/app.ts
+++ 05Function/app.ts
@@ -2,4 +2,22 @@ function Foo(a:number, b:number) {
     return [a+b];
 };
 
-console.log(Foo(12,5));
+interface IGetData {
+    completed: boolean;
+    id: number;
+    title: string;
+    userId: number;
+};
+
+
+async function getData(url:string): Promise<IGetData> {
+    const req = await fetch(url);
+    if(!req.ok) {
+        throw new Error(`could not ${url}`);
+    }
+    return await req.json();
+};
+
+getData('https://fakestoreapi.com/prodsucts/1')
+    .then(data => console.log(data));
+    
